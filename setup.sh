ed12949fc5df21ee1666c7da9a0edf9e7bf333b1
camembert-t <taka.k.fumi@gmail.com>
2024-08-19 07:17:59 +0000

    add fzf and z

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ ln -s ~/.myconfig/dotfiles2/zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
 
 exec zsh
 
@@ -24,3 +25,4 @@ sudo apt-get install -y tmux
 ln -s ~/.myconfig/dotfiles2/tmux.conf ~/.tmux.conf
 sudo apt-get install -y universal-ctags
 sudo apt-get install -y silversearcher-ag
+sudo apt-get install -y fzf
--- zshrc
+++ zshrc
@@ -70,7 +70,7 @@ ZSH_THEME="mytheme"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search z)
 
 source $ZSH/oh-my-zsh.sh
 
